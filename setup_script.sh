05e87b795e9b643e59795ec7db500d59a1f6d8e5
Yağız Alp Ersoy <129625653+Sokisati@users.noreply.github.com>
2024-08-28 23:13:20 +0300

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y git python3-venv python3-pip i2c-tools && sudo apt-get install pigpio -y && sudo systemctl start pigpiod && sudo systemctl enable pigpiod
+
+python3 -m venv epl_env
+source epl_env/bin/activate
+
+pip install RPi.GPIO bme680 gpsd-py3 gpiozero pigpio mpu9250-jmdev pillow numpy
+
+git clone https://github.com/Sokisati/st_epl && git clone https://github.com/Sokisati/camera_filter_epl
+
+
