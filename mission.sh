658ed1e9c3f95b2eb5e721cebf0f2aff7c3207db
Yağız Alp Ersoy <129625653+Sokisati@users.noreply.github.com>
2024-08-29 04:35:13 +0300

    Update mission.sh

--- mission.sh
+++ mission.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python3 st_epl/src/st_epl.py && python3 camera_filter_epl/src/camera_filter_epl.py && python3 camera_epl/src/camera_epl.py
+python3 st_epl/src/st_epl.py &
+python3 camera_filter_epl/src/camera_filter_epl.py &
+python3 camera_epl/src/camera_epl.py &
+
